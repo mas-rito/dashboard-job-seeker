5a1ae9988d401087d4abb1aea8035fdf46bf6738
mas-rito <ritoramadhan8@gmail.com>
2024-03-01 14:12:52 +0700

    feat(lib): init supabase and create function supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,59 @@
+import { createClient } from "@supabase/supabase-js";
+
+// Create a single supabase client for interacting with your database
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!!,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY!!
+);
+
+export const supabaseUploadFile = async (
+  file: File | string,
+  fileName: string,
+  bucket: "company" | "applicant"
+) => {
+  const { data, error } = await supabase.storage
+    .from(bucket)
+    .upload(fileName, file, {
+      cacheControl: "3600",
+      upsert: false,
+    });
+
+  return { data, error };
+};
+
+export const supabaseGetFileUrl = async (
+  fileName: string,
+  bucket: "company" | "applicant"
+) => {
+  const { data } = supabase.storage
+    .from(bucket)
+    .getPublicUrl("folder/" + fileName);
+
+  return { data };
+};
+
+export const supabaseDeleteFile = async (
+  fileName: string,
+  bucket: "company" | "applicant"
+) => {
+  const { error } = await supabase.storage
+    .from(bucket)
+    .remove(["folder/" + fileName]);
+
+  return { error };
+};
+
+export const supabaseUpdateFile = async (
+  file: File | string,
+  fileName: string,
+  bucket: "company" | "applicant"
+) => {
+  const { data, error } = await supabase.storage
+    .from(bucket)
+    .update("public/" + fileName, file, {
+      cacheControl: "3600",
+      upsert: true,
+    });
+
+  return { data, error };
+};
